5b1635beeed1d74147bfb2b20ac0cd27f3f0392e
Guilherme Avelino <guilhermeavelino1@outlook.com>
2020-12-17 01:59:22 -0300

    Configuring migrations

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-    return res.json({
-        ok: false
-    })
+routes.get("/", async (req, res) => {
+    const user = await User.create({
+        name: "Guilherme",
+        email: "guilherme.avelino@recode.org.br",
+        password_hash: "12345"
+    });
+    
+    return res.json(user)
 })
 
 export default routes;
